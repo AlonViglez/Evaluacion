88430baaf281fa713e13365d28a42d20e79da6d2
AlonViglez <jesusalonsovilla2002@outlook.com>
2022-09-30 11:02:08 -0500

    Agrego el problema2

---
+++ problema2.c
@@ -0,0 +1,40 @@
+/*Jesus Alonso Villa Gonzalez 3D
+ *Ingenieria de Software
+ *Estructura de Datos
+ */
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+int recursiva(int i)
+  {
+   int res=0;
+   if(i>0)
+     {
+   	return res=3*pow(5,i-1); 
+     }
+   else
+     {
+   	return 0;
+     }
+  }
+
+
+int main()
+  {
+   int n=0;
+   int res=0;
+   printf("Ingrese el numero de iteraciones: ");
+   scanf("%d",&n);
+
+   for(int i=1; i<=n; i++)
+     {
+      res=recursiva(i);
+      printf("%d,",res);
+     }
+   printf("\n");
+   printf("Numero de repeticiones = %d\n",n);
+   return 0;
+  }
